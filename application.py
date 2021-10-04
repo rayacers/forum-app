086532077d5fa8650a176281b1b7a93b522a9469
Po-Jui Chang <rayacers@gmail.com>
2021-10-04 09:48:36 +0200

    Modify application.py

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 from website import create_app
 
-if __name__== "__main__":
-    application = create_app()
-    application.run(host='0.0.0.0', debug = True)
+application = create_app()
+application.run(debug = True)
